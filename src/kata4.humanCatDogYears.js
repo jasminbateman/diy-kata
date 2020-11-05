f21f9c2220f04d66df59763e520bdc162e266d94
Jasmin Bateman <jasminbateman@gmail.com>
2020-11-05 21:17:20 +0000

    completed humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,78 @@
-const humanCatDogYears = number => {};
+    const humanCatDogYears = number => {
 
-module.exports = humanCatDogYears;
+        if (typeof number !== "number") {
+            return "this is not a number";
+        }
+
+        if (number < 1) {
+            return "please enter a valid number";
+        }
+
+        let cat;
+        let dog;
+
+        if (number === 1) {
+            cat = 15;
+            dog = 15;
+        }
+
+        else if (number === 2) {
+            cat = 15 + 9;
+            dog = 15 + 9;
+        }
+
+        else {
+            cat = 15 + 9 + ((number - 2) * 4);
+            dog = 15 + 9 + ((number - 2) * 5);
+        }
+
+        return [number, cat, dog];
+    };
+
+    module.exports = humanCatDogYears;
+
+
+// const humanCatDogYears = number => {
+
+//     const catYears = years => {
+//         if (number === 1) {
+//             catYears = 15;
+//         }
+
+//         else if (number === 2) {
+//             catYears = (15 + 9);
+//         }
+
+//         else if (number > 2) {
+//             catYears = (((number - 2) * 4) + 15 + 9);
+//         }
+//     }
+//     /*  
+//         > if number === 1 then catYears is 15
+//         > if number === 2 then catYears is 15 + 9
+//         > if number >2 then cat years is 15 + 9 + ((number - 2) * 4)
+//     */
+
+//     const dogYears = dogYearsCalc => {
+//         if (number === 1) {
+//             dogYears = 15;
+//         }
+
+//         else if (number === 2) {
+//             dogYears = (15 + 9);
+//         }
+
+//         else if (number > 2) {
+//             dogYears = (((number - 2) * 5) + 15 + 9);
+//         }
+//     }
+//     /*  
+//         > if number === 1 then dogYears is 15
+//         > if number === 2 then dogYears is 15 + 9
+//         > if number >2 then dogYears is 15 + 9 + ((number - 2) * 5)
+//     */
+
+//     return ([number, catYears, dogYears])
+// };
+
+// module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,20 @@
 const { humanCatDogYears } = require("../src");
 
+describe ("humanCatDogYears", () => {
+    it("returns an array of human, cat and dog years when passed human years", () => {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+        expect(humanCatDogYears(24)).toEqual([24, 112, 134]);
+
+    });
+
+    it("returns error message if not a number", () => {
+        expect(humanCatDogYears("hello")).toEqual("this is not a number");
+    })
+
+    it("returns error if number is below 1", () => {
+        expect(humanCatDogYears(-2)).toEqual("please enter a valid number1");
+    })
+});
+
 // Look Ma, no handlebars!!!
